036a46deea3f722eaa79253f76c66004b1d5638c
tzachi81 <tzahih@gmail.com>
2016-11-29 16:30:03 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# React Redux bookstore
+
+```
+	> npm install
+	> npm start
+```
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,7 @@
+export default function(){
+    return
+    [
+        {title: 'Alone in Berlin'},
+        {title: 'The Death of Bunny Monroe'}
+    ]
+}
