1415084379788429f0fa162a0611802b2003ad99
tzachi81 <tzahih@gmail.com>
2016-11-30 00:53:15 +0200

    add action: list item click, to show book details

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,6 +1,7 @@
 export default function(){
     return [
-        {title:  'Alone in Berlin'},
-        {title:  'The Death of Bunny Monroe'}
+        {title:  "Alone in Berlin", author: "Hans Falada"},
+        {title:  "The Death of Bunny Monroe", author: "Nick Cave"},
+        {title:  "The Strain", author: "Guillermo Del Toro"}
     ]
 }
