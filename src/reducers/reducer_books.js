480683a1b34571a3144e0116c05344baed7520b7
tzachi81 <tzahih@gmail.com>
2016-11-30 11:04:22 +0200

    fix books object

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,7 +1,10 @@
 export default function(){
     return [
-        {title:  "Alone in Berlin", author: "Hans Falada"},
-        {title:  "The Death of Bunny Monroe", author: "Nick Cave"},
-        {title:  "The Strain", author: "Guillermo Del Toro"}
+        {title:  "Alone in Berlin", author: "Hans Fallada"},
+        {title:  "The Death of Bunny Munro", author: "Nick Cave"},
+        {title:  "The Strain", author: "Guillermo Del Toro"},
+        {title:  "The Alliance", author: "Jolina Petersheim"},
+        {title:  "The Dark Blood", author: "A.J. Smith"},
+        {title:  "Apes and Angels", author: "Ben Bova"}
     ]
 }
